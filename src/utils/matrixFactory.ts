2fc540565dab3374b1cbefe7d6a655ec8d7b1007
Rodrigo Vallejos <42015094+awakenedhaki@users.noreply.github.com>
2024-01-21 17:25:03 -0800

    Implement createGivensRotationMatrix factory function

--- src/utils/matrixFactory.ts
+++ src/utils/matrixFactory.ts
@@ -17,3 +17,31 @@ export function createIdentityMatrix(dim: number): Matrix {
 
     return matrix;
 }
+
+/**
+ * Creates a Givens rotation matrix of the specified dimension.
+ * 
+ * @param dim - The dimension of the matrix.
+ * @param i - The row index of the first element to be rotated.
+ * @param j - The row index of the second element to be rotated.
+ * @param theta - The angle of rotation in radians.
+ * @returns The Givens rotation matrix.
+ * @throws Error if the row index of the second element is greater than or equal to the row index of the first element.
+ */
+export function createGivensRotationMatrix(dim: number, i: number, j: number, theta: number): Matrix {
+    if (i >= j) {
+        throw new Error("The row index of the second element to be rotated must be less than the row index of the first element to be rotated.");
+    }
+
+    const matrix: Matrix = createIdentityMatrix(dim);
+
+    const s: number = Math.sin(theta);
+    const c: number = Math.cos(theta);
+
+    matrix.set(i, i, c);
+    matrix.set(i, j, s);
+    matrix.set(j, j, c);
+    matrix.set(j, i, -s);
+
+    return matrix;
+}
