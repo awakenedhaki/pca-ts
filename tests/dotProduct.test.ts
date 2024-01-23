97a4fb0f218603c3bc870428b36e191157686709
Rodrigo Vallejos <42015094+awakenedhaki@users.noreply.github.com>
2024-01-23 14:54:04 -0800

    Add strassen method for dot product

--- tests/dotProduct.test.ts
+++ tests/dotProduct.test.ts
@@ -1,6 +1,7 @@
 import Matrix from "../src/classes/matrix";
 import dotProduct from "../src/operations/dotProduct";
 import naive from "../src/operations/dotProductMethods/naive";
+import strassen from "../src/operations/dotProductMethods/strassen";
 
 describe("dot product", () => {
     let A: Matrix;
@@ -20,17 +21,43 @@ describe("dot product", () => {
         });
     });
 
-    describe.each([
-        { dataA: [[1, 2, 3], [4, 5, 6]], dataB: [[7, 8], [9, 10], [11, 12]], dataC: [[58, 64], [139, 154]]},
-        { dataA: [[7, 8], [9, 10], [11, 12]], dataB: [[1, 2, 3], [4, 5, 6]], dataC: [[39, 54, 69], [49, 68, 87], [59, 82, 105]]},
-    ])("Valid Input", ({ dataA, dataB, dataC }) => {
-        beforeEach(() => {
-            A = Matrix.from2DArray(dataA);
-            B = Matrix.from2DArray(dataB);
-        });
+    describe("Valid Input", () => {
+        let A: Matrix;
+        let B: Matrix;
 
-        test("naive", () => {
-            expect(naive(A, B).to2DArray()).toEqual(dataC);
+        describe.each([
+            { dataA: [[1, 2, 3], [4, 5, 6]], dataB: [[7, 8], [9, 10], [11, 12]], dataC: [[58, 64], [139, 154]] },
+            { dataA: [[7, 8], [9, 10], [11, 12]], dataB: [[1, 2, 3], [4, 5, 6]], dataC: [[39, 54, 69], [49, 68, 87], [59, 82, 105]] },
+        ])("Non-square Matrices", ({ dataA, dataB, dataC }) => {
+            beforeEach(() => {
+                A = Matrix.from2DArray(dataA);
+                B = Matrix.from2DArray(dataB);
+            });
+
+            test("naive", () => {
+                expect(naive(A, B).to2DArray()).toEqual(dataC);
+            });
+
+            test.skip.failing("strassen", () => {
+                expect(strassen(A, B).to2DArray()).toEqual(dataC);
+            });
+        });
+    
+        describe.each([
+            { dataA: [[1, 2], [3, 4]], dataB: [[5, 6], [7, 8]], dataC: [[19, 22], [43, 50]] },
+        ])("Square Matrices", ({ dataA, dataB, dataC }) => {
+            beforeEach(() => {
+                A = Matrix.from2DArray(dataA);
+                B = Matrix.from2DArray(dataB);
+            });
+
+            test("naive", () => {
+                expect(naive(A, B).to2DArray()).toEqual(dataC);
+            });
+
+            test("strassen", () => {
+                expect(strassen(A, B).to2DArray()).toEqual(dataC);
+            });
         });
     });
 });
